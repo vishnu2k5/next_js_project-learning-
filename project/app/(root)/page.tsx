7dc0cf900acc82d47666da166bb90d6a90d1e683
vishnu2k5 <mvishnuvardhan156@gmial.com>
2025-06-27 09:25:26 +0530

    search form

--- project/app/(root)/page.tsx
+++ project/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import SearchForm from '../components/SearchForm'
 
 const page = () => {
   return (
@@ -8,12 +9,14 @@ const page = () => {
     backgroundColor: "#FF2E63",
     backgroundImage:
       "linear-gradient(to right, transparent 49.5%, rgba(251, 232, 67, 0.2) 49.5%, rgba(251, 232, 67, 0.6) 50.5%, transparent 50.5%)",
-    backgroundSize: "5% 100%",
+    backgroundSize: "5vw 100%",
     backgroundRepeat: "repeat-x",
     backgroundPosition: "center",
-    height: "100px",
+    height: "50vh",
   }}className='w-full bg-white min-h-[530px] flex justify-center items-center flex-col py-10 px-6;'>
-      <h1 className='text-3xl heading'>home</h1>
+      <h1 className='text-3xl heading'>share your thing, <br />connect with your famaly</h1>
+      <p>share, connect, explore....</p>
+      <SearchForm />
      </section>
     </div>
   )
---
+++ project/app/components/SearchForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchForm = () => {
+  return (
+    <div>
+      searchForm
+    </div>
+  )
+}
+
+export default SearchForm

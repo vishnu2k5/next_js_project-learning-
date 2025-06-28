7dd7d08dd45c1ff80ed7636206ad5a40107e4d09
vishnu2k5 <mvishnuvardhan156@gmial.com>
2025-06-28 14:42:31 +0530

    cummit

--- project/app/(root)/page.tsx
+++ project/app/(root)/page.tsx
@@ -11,7 +11,7 @@ const page = async({searchParams}:{
     {
       _createdAt: new Date(),
       views:55,
-      auther:{_id:1},
+      author:{_id:1, name: 'visnu'},
       _id:1,
       description:'adfafasfasfasfasfasf',
       image:'https://images.unsplash.com/photo-1589254066007-898d52d910d3?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
--- project/components/StartupCard.tsx
+++ project/components/StartupCard.tsx
@@ -1,23 +1,34 @@
 import { formateDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 import React from 'react'
+import Link from 'next/link'
 
 const StartupCard = ( {post}:{post:StartupTypeCard}) => {
 
+    const { _createdAt, views, author:{_id:authorid, name},image, title, _id , category} = post
+
   return (
-    <li className='bg-white border-[5px] border-black py-6 px-5 rounded-[22px] shadow-200 hover:border-primary transition-all duration-500 hover:shadow-300 hover:bg-primary-100'>
-        <div className='flex-between'>
-            <p className='font-medium text-[16px] bg-primary-100 px-4 py-2 rounded-full group-hover:bg-white-100 text-black'>
-                {formateDate(post._createdAt)}
+    <li className='bg-white border-[5px] border-black py-6 px-5 rounded-[22px] shadow-200 hover:border-[#F7F7F7] transition-all duration-500 hover:shadow-300 hover:bg-[#FFE8F0]'>
+        <div className='flex gap-25'>
+            <p className='font-medium text-[16px] bg-[#FFE8F0]-100 px-4 py-2 rounded-full group-hover:bg-[#F7F7F7] text-black'>
+                {formateDate(_createdAt)}
             </p>
-            <div className='flex gap-1.5'>
-                <EyeIcon className='size-6 text-black'></EyeIcon>
+            <div className='flex gap-1.5 px-4 py-2'>
+                <EyeIcon className='size-6 text-[#EE2B69]'></EyeIcon>
                 <span className='text-16-medium text-black'>
-                    {post.views}
+                    {views}
                 </span>
             </div>
         </div>
-        <div>
+        <div className='flex-between mt-5 gap-5'>
+            <div className='fles-1'>
+                <Link href={`/user/${authorid}`}>
+                <p className='text-16-medium line-clamp-1 text-black'>{name}</p>
+                </Link>
+                <Link href={`/satrtup/${_id}`}>
+                <h2 className='text-26 font-bold text-black line-clamp-1'>{title}</h2></Link>
+
+            </div>
 
         </div>
 
